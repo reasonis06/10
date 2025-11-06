f40a63f7c8de4248d82b3a2a08a000c7d782bf92
이성은 <katrina060208@gmail.com>
2025-11-06 15:14:22 +0900

    실습1

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char code1 = 'A'; // char type variable
+	int code2 = 65; //int type variable
+  
+  
+  printf("code1 = %c, (%d)\n", code1, code1);
+  printf("code2 = %c, (%d)\n", code2, code2);
+  
+  system("PAUSE");	
+  return 0;
+}
+
+// code1 = A, (65)
+// code2 = A, (65)
